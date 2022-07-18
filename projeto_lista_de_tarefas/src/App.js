496cee4f5dc3067cb81711491a0633bc94224461
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-07-18 17:17:03 -0300

    Add files via upload

---
+++ projeto_lista_de_tarefas/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Main from './components/Main'
+import './App.css';
+
+export default function App() { // esta função retorna um códig similar ao HTML, chamado JSX.
+  return <Main />;
+}
+
